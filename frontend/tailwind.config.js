d572defa43f9276487808bae5bb7e2beb890d025
vssemwal2004 <422semwalvivek@gmail.com>
2025-10-08 14:41:49 +0530

    dfg

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all JS/JSX/TS/TSX files in src
+    "./public/index.html", // Include HTML files if needed
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
